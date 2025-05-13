73e18870ae01a0037aa00b65f80e68e05116bc6e
Oscar-Wohlfarhrt <oscarwohlfarhrt@gmail.com>
2025-05-13 13:00:22 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DSRemapper.ServerApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
