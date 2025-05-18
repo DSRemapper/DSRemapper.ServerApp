02fac2eefd21717bd5aa0c06c8ac08a7167971cc
Oscar-Wohlfarhrt <oscarwohlfarhrt@gmail.com>
2025-05-18 12:20:15 -0300

    Fast update

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Formats.Asn1;
+﻿using System.Net.Mime;
+using System.Text.Json;
 using FireLibs.Web.Http;
 using Microsoft.Extensions.Logging;
 
@@ -13,18 +14,41 @@ namespace DSRemapper.ServerApp
 
         static HttpServer MainServer = new(5100, 60000, logFac.CreateLogger<HttpServer>());
         static bool run = true;
+
+        static readonly string wwwrootPath = Path.GetFullPath("./wwwroot");
+
+        static Dictionary<string, string> Devices = new()
+        {
+            {"id1", "test1" },
+            {"id2", "test2" },
+            {"id3", "test3" },
+            {"id4", "test4" },
+        };
+
         static void Main(string[] args)
         {
             logger.LogInformation("Configuring server");
             AppDomain.CurrentDomain.DomainUnload += ProcessExit;
             MainServer.OnProcessRequest = MainServer_RequestHandler;
 
-            MainServer.MapGetToFile("/", "./wwwroot/mainMenu.html");
+            MainServer.MapGet("/devices",(req, opts)=>
+            {
+                if (opts.Length == 0)
+                    return new(HttpDefaultStatus.OK, [],
+                        JsonSerializer.Serialize(Devices.Keys),
+                        MediaTypeNames.Application.Json);
+                if(Devices.TryGetValue(opts[0], out var dev))
+                    return new(HttpDefaultStatus.OK, [],
+                        JsonSerializer.Serialize(dev),
+                        MediaTypeNames.Application.Json);
+
+                return HttpResponse.NotFoundResponse;
+            });
 
             logger.LogInformation("Starting server");
             MainServer.Start();
-            
-            while(run);
+
+            while (run) ;
         }
 
         private static void ProcessExit(object? sender, EventArgs e)
@@ -32,22 +56,21 @@ namespace DSRemapper.ServerApp
             MainServer.Stop();
         }
 
-        static Dictionary<string,Dictionary<string,Func<HttpRequest,string,HttpResponse>>> registeredOperations = new(){
-            {"GET", new(){
-                {"/devices", (req,path)=>{
-                    return HttpResponse.NotImplementedResponse;
-                }},
-            }},
-        };
-
         static HttpResponse MainServer_RequestHandler(HttpRequest request){
             HttpResponse response = new(HttpDefaultStatus.OK,[],"");
             
-            if(request.Path == "/exit.exit"){
+            if(request.Path == "/exit.exit")
                 run = false;
-            }
             else if(request.IsOperation("GET")){
-                response.SetContent("This is a test");
+                string fullPath = Path.GetFullPath(Path.Combine(wwwrootPath, request.Path[1..]));
+                //Console.WriteLine($"{wwwrootPath} | {fullPath}");
+                if (!fullPath.StartsWith(wwwrootPath))
+                    return new(HttpDefaultStatus.Forbidden, [], "");
+
+                if (!File.Exists(fullPath))
+                    return new(HttpDefaultStatus.NotFound, [], "");
+
+                response.SetContentFromFile(fullPath);
                 return response;
             }
 
